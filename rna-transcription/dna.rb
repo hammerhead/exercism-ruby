33e532e5334eeff90061418c77b10467af17aaa8
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-08-28 15:50:00 +0200

    Make tests pass

--- rna-transcription/dna.rb
+++ rna-transcription/dna.rb
@@ -1,19 +1,30 @@
 class DNA
   attr_reader :dna
 
-  NUCLEOTIDES = ['A', 'C', 'G', 'T']
+  DNA_NUCLEOTIDES = ['A', 'C', 'G', 'T']
+  RNA_NUCLEOTIDES = ['A', 'C', 'G', 'U']
+
+  URACIL = 'U'
 
   def initialize(dna)
     @dna = dna
   end
 
+  def nucleotides
+    DNA_NUCLEOTIDES + RNA_NUCLEOTIDES
+  end
+
+  def is_valid?(nucleotide)
+    nucleotides.include? nucleotide
+  end
+
   def count(nucleotide)
-    raise ArgumentError.new 'Invalid nucleotide' unless NUCLEOTIDES.include? nucleotide
+    raise ArgumentError.new 'Invalid nucleotide' unless is_valid? nucleotide
     dna.chars.count nucleotide
   end
 
   def nucleotide_counts
-    NUCLEOTIDES.each_with_object(Hash.new(0)).each do |nucleotide, count|
+    (nucleotides - [URACIL]).each_with_object(Hash.new(0)).each do |nucleotide, count|
       count[nucleotide] = count nucleotide
     end
   end
